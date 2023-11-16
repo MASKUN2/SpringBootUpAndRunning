05823fb9ad1f4f3835e68ab2f831082ce380d0ed
MASKUN2 <maskunflower2@naver.com>
2023-11-16 20:41:20 +0900

    초기커밋

---
+++ src/main/java/com/example/springbootupandrunning/SpringBootUpAndRunningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootupandrunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpAndRunningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUpAndRunningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootupandrunning/SpringBootUpAndRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootupandrunning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpAndRunningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
